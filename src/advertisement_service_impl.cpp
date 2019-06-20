a7368170b4aaea07a677a9151ceeeccc1c87c0e2
jinshang <jinshang@tencent.com>
2019-06-20 18:26:57 +0800

    Finished frameword, inter-process communication to be added 完成了基本框架，需要加入进程间通讯

--- src/advertisement_service_impl.cpp
+++ src/advertisement_service_impl.cpp
@@ -11,150 +11,15 @@
 #include "advertisement_server_config.h"
 #include "ftrl.pb.h"
 
-#include "include/LR.h"
-#include "include/corpus.h"
-#include "include/sparse_vector.h"
-#include <iostream>
-#include <cmath>
-#include <string>
-#include <iomanip>
-#include <omp.h>
-
-using namespace std;
-
-inline int sgn(double a){
-	return (0<a)-(a>0);
+int RequestAdId(::google::protobuf::int64 user_id){
+	//request for result
+	int ad_id = 0;
+	return ad_id;
 }
 
-class FTRL
-{
-private:
-	//Number of Features
-	int d;
-
-	//Parameters
-	double alpha;
-	double beta;
-	double l1;
-	double l2;
-
-	//Decision Function
-	LR lr;
-
-	//Model
-	SpVec w;
-	SpVec z;
-	SpVec n;
-
-public:
-	FTRL():alpha(0.5),beta(1.0),l1(1.0),l2(1.0),d(4){};
-
-	FTRL(int D, double Alpha, double Beta, double L1, double L2):d(D),alpha(Alpha),beta(Beta),l1(L1),l2(L2){};
-
-	double perdict(SpVec& x){
-		return lr.decision(w,x);
-	}
-
-	double update(SpVec& x, double y){
-		#pragma omp parallel for
-		for(int i = 0;i < d;++i){
-			if(abs(z.get_value(i)) > l1){
-				#pragma omp critical
-				w.set_value(i,-1.0/((beta+sqrt(n.get_value(i)))/alpha+l2)*(z.get_value(i)-sgn(z.get_value(i))*l1));
-			}
-			else{
-				#pragma omp critical
-				w.set_value(i,0);
-			}
-		}
-		double p = perdict(x);
-		SpVec g = lr.gradient(p,y,x);
-
-		#pragma omp parallel for
-		for(int i = 0;i < d;i++){
-			double temp_g = (p-y)*(x.get_value(i));
-			g.set_value(i, temp_g);
-			double sigma = (1.0/alpha)*(sqrt(n.get_value(i)+(temp_g*temp_g))-sqrt(n.get_value(i)));
-			double temp_z = z.get_value(i)+temp_g-sigma*w.get_value(i);
-			double temp_n = n.get_value(i)+temp_g*temp_g;
-			
-			#pragma omp critical
-			z.set_value(i,temp_z);
-			#pragma omp critical
-			n.set_value(i,temp_n);
-		}
-
-		return lr.loss(p,y);
-	}
-
-	void train(corpus& data){
-		cout<<"training "<<data.size()<<" data"<<endl;
-		double correct = 0;
-		double wrong = 0;
-
-		for(int i=0;i<data.size();i++){
-			cout<<"data "<<i<<" ";
-			int p = (perdict(data[i].x)>0.5);
-			double loss = update(data[i].x,data[i].y);
-
-			if(p==data[i].y){
-				correct++;
-			}
-			else{
-				wrong++;
-			}
-			cout<<"loss: "<< loss <<" ";
-			cout<<"accuracy: "<<correct/(correct+wrong)<<endl;
-
-
-
-		}
-		cout<<"trained weight:"<<endl;
-		w.print_value();
-	}
-
-	double test(corpus& data){
-		cout<<"testing "<<data.size()<<" data"<<endl;
-		double correct = 0;
-		double wrong = 0;
-
-		#pragma omp parallel for
-		for(int i=0;i<data.size();i++){
-
-			// cout<<"data "<<i<<":"<<perdict(data[i].x)<<"--"<<data[i].y<<endl;
-			int p=0;
-			if(perdict(data[i].x)>0.5){
-				p = 1;
-			}
-			if(p==data[i].y){
-				correct++;
-			}
-			else{
-				wrong++;
-			}
-		}
-		return correct/(correct+wrong);
-	}
-
-	void save(ofstream* FILE){
-		sp_iter it = w.vc.begin();
-		while(it!=w.vc.end()){
-			(*FILE)<<it->first<<": "<<it->second<<endl;
-			it++;
-		}
-	}
-
-	void load(ifstream* FILE){
-		string line;
-		while(getline(*FILE,line)){
-			vector<string> temp = parse_feature(line,":");
-			
-			long x = stoi(temp[0]);
-			double v = stod(temp[1]);
-			w.set_value(x,v);
-		}
-	}
-};
+void SendFeedback(::google::protobuf::int64 ad_id){
+	//send feedback for training 
+}
 
 AdvertisementServiceImpl::AdvertisementServiceImpl(ServiceArgs_t &app_args) : args_(app_args) {
 }
@@ -168,10 +33,13 @@ int AdvertisementServiceImpl::PHXEcho(const google::protobuf::StringValue &req, 
 }
 
 int AdvertisementServiceImpl::Advertisement(const ftrl::AdvertisementRequest &req, ftrl::AdvertisementResult *resp) {
-    return -1;
+    int ad_id = RequestAdId(req.user_id());//to be implemented
+    resp->set_ad_id((::google::protobuf::int64) ad_id);
+    return 0;
 }
 
 int AdvertisementServiceImpl::Feedback(const ftrl::AdvertisementFeedback &req, google::protobuf::Empty *resp) {
-    return -1;
+    SendFeedback(req.ad_id(),req.feedback());
+    return 0;
 }
 
